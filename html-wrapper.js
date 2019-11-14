4d3db513b8cc10273d51a59ebaf6c78da44265be
Josh Zeltman <joshua.zeltman@gmail.com>
2019-11-14 20:37:03 -0300

    Adding components, adding gitignore, package.json

---
+++ html-wrapper.js
@@ -0,0 +1,17 @@
+import { Component } from 'rreact';
+
+export default class HTMLWrapper extends Component {
+    constructor() {
+        super();
+        this.getMarkup.bind(this);
+        this.state = { html: null };
+    }
+
+    getMarkup(prop) {
+        return { __html: prop };
+    }
+
+    render() {
+        return <div dangerouslySetInnerHTML={this.getMarkup(this.props.html)} />;
+    }
+};
