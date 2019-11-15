40b74f2e691ae44ea6de31fc6e813b0e4e30de43
Josh Zeltman <joshua.zeltman@gmail.com>
2019-11-15 09:52:04 -0300

    Added index.js to import/export the components

---
+++ index.js
@@ -0,0 +1,7 @@
+const AsyncWrapper = require("./async-wrapper");
+const HTMLWrapper = require("./html-wrapper");
+
+module.exports = { 
+    AsyncWrapper: AsyncWrapper,
+    HTMLWrapper: HTMLWrapper
+}
